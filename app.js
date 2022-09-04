ae0070488a8b37efcaa5c8efd7fdac5818c0926a
Gonzalo Gatti <gonzagatti@gmail.com>
2022-09-04 10:41:00 -0300

    Actualizado

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ require('dotenv').config();
 
 app.get('/', (req,res)=>{
     console.log("Peticion recibida");
-    res.send("Hola mundo sin .env")
+    res.send("Hola mundo")
 })
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT;
 
 app.listen(PORT, ()=>{
-    console.log(`App escuchando en el puert ${PORT}`)
+    console.log(`App escuchando en el puerto ${PORT}`)
 })
